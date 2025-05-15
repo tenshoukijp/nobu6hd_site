c9b28adb7b6cb78df14bcac563ded430179fbe4f
tenshoukijp <komiyama_a@hotmail.com>
2025-05-15 15:40:43 +0900

    こみっと

--- sitemap.bat
+++ sitemap.bat
@@ -1,5 +1,5 @@
 curl -s https://hd.xn--rssu31gj1g.jp/sitemap.php
 timeout /t 5
 
-cmd /C C:/usr/nextftp/NEXTFTP.EXE $Host17 -local="G:/repogitory_jp/nobu6hd_site" -quit -nosound -minimize -download=sitemap.xml -nokakunin
+cmd /C C:/usr/nextftp/NEXTFTP_CLI.EXE $Host17 -local="G:/repogitory_jp/nobu6hd_site" -quit -nosound -minimize -download=sitemap.xml -nokakunin
 
